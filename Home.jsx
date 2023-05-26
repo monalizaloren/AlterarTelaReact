1391d789c68e11e25768423344fb15f1e98d9683
Monaliza Loren <83433815+monalizaloren@users.noreply.github.com>
2023-05-26 08:38:16 -0300

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+const Home = () => {
+return (
+	<div>
+	<h1>Home Page</h1>
+	<br />
+	
+	</div>
+);
+};
+
+export default Home;
